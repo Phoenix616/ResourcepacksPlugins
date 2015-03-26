da1b87a593c9048817cd7aa15c6a08400a220d2f
Max Lee <Phoenix616@users.noreply.github.com>
2015-03-26 18:49:37 +0100

    Track ServerConnectListener

---
+++ src/main/java/de/themoep/BungeeResourcepacks/bungee/listeners/ServerConnectListener.java
@@ -0,0 +1,27 @@
+package de.themoep.BungeeResourcepacks.bungee.listeners;
+
+import de.themoep.BungeeResourcepacks.bungee.BungeeResourcepacks;
+import de.themoep.BungeeResourcepacks.core.ResourcePack;
+import net.md_5.bungee.api.event.ServerConnectEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+/**
+ * Created by Phoenix616 on 24.03.2015.
+ */
+public class ServerConnectListener implements Listener {
+    
+    @EventHandler
+    public void onServerConnect(ServerConnectEvent event) {
+        ResourcePack pack = BungeeResourcepacks.getInstance().getPackManager().getServerPack(event.getTarget().getName());
+        if(pack == null) {
+            pack = BungeeResourcepacks.getInstance().getPackManager().getServerPack("!global");
+        }
+        if(pack != null) {
+            ResourcePack prev = BungeeResourcepacks.getInstance().getPackManager().getUserPack(event.getPlayer().getUniqueId());
+            if(prev != null && !pack.equals(prev)) {
+                BungeeResourcepacks.getInstance().setPack(event.getPlayer(), pack);
+            }
+        }
+    }
+}
