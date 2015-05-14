b4f034d3cac28573a35a6aecc171e31d9ffb9977
Max Lee <Phoenix616@users.noreply.github.com>
2015-05-14 22:52:28 +0200

    Fix player not able to approve pack by adding a slight delay

--- src/main/java/de/themoep/BungeeResourcepacks/bungee/listeners/ServerSwitchListener.java
+++ src/main/java/de/themoep/BungeeResourcepacks/bungee/listeners/ServerSwitchListener.java
@@ -2,10 +2,15 @@ package de.themoep.BungeeResourcepacks.bungee.listeners;
 
 import de.themoep.BungeeResourcepacks.bungee.BungeeResourcepacks;
 import de.themoep.BungeeResourcepacks.core.ResourcePack;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.connection.Server;
 import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.event.ServerSwitchEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Phoenix616 on 24.03.2015.
@@ -16,19 +21,30 @@ public class ServerSwitchListener implements Listener {
     public void onServerSwitch(ServerSwitchEvent event) {
         BungeeResourcepacks plugin = BungeeResourcepacks.getInstance();
         if(!plugin.enabled) return;
-        
-        ResourcePack pack = plugin.getPackManager().getServerPack(event.getPlayer().getServer().getInfo().getName());
-        if(pack == null) {
-            pack = plugin.getPackManager().getGlobalPack();
-        }
-        if(pack == null && plugin.getPackManager().getUserPack(event.getPlayer().getUniqueId()) != null) {
-            pack = plugin.getPackManager().getEmptyPack();
-        }
-        if(pack != null) {
-            ResourcePack prev = plugin.getPackManager().getUserPack(event.getPlayer().getUniqueId());
-            if(!pack.equals(prev)) {
-                plugin.setPack(event.getPlayer(), pack);
+
+        final UUID playerid = event.getPlayer().getUniqueId();
+        plugin.getProxy().getScheduler().schedule(plugin, new Runnable() {
+            @Override
+            public void run() {
+                BungeeResourcepacks plugin = BungeeResourcepacks.getInstance();
+                ProxiedPlayer player = plugin.getProxy().getPlayer(playerid);
+                if(player != null) {
+                    ResourcePack pack = null;
+                    Server server = player.getServer();
+                    if(server != null) {
+                        pack = plugin.getPackManager().getServerPack(server.getInfo().getName());
+                    }
+                    if(pack == null) {
+                        pack = plugin.getPackManager().getGlobalPack();
+                    }
+                    if(pack == null && plugin.getPackManager().getUserPack(playerid) != null) {
+                        pack = plugin.getPackManager().getEmptyPack();
+                    }
+                    if(pack != null) {
+                        plugin.setPack(player, pack);
+                    }
+                }
             }
-        }
+        }, 300L, TimeUnit.MILLISECONDS);
     }
 }
