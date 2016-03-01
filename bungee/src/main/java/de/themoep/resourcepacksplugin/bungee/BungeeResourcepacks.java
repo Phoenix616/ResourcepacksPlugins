8d0944a36b33e70af0c1a043a481b565c1fab07c
Max Lee <Phoenix616@users.noreply.github.com>
2016-03-01 15:53:13 +0100

    Revert "1.9 changed the protocol, combat that"
    This reverts commit 63e8f1dc4c03df0c8b17c31987663ca9b584087d.

--- bungee/src/main/java/de/themoep/resourcepacksplugin/bungee/BungeeResourcepacks.java
+++ bungee/src/main/java/de/themoep/resourcepacksplugin/bungee/BungeeResourcepacks.java
@@ -5,8 +5,6 @@ import com.google.common.io.ByteStreams;
 import de.themoep.resourcepacksplugin.bungee.listeners.DisconnectListener;
 import de.themoep.resourcepacksplugin.bungee.listeners.ServerSwitchListener;
 import de.themoep.resourcepacksplugin.bungee.packets.ResourcePackSendPacket;
-import de.themoep.resourcepacksplugin.bungee.packets.ResourcePackSendPacket18;
-import de.themoep.resourcepacksplugin.bungee.packets.ResourcePackSendPacket19;
 import de.themoep.resourcepacksplugin.core.PackManager;
 import de.themoep.resourcepacksplugin.core.ResourcePack;
 import de.themoep.resourcepacksplugin.core.ResourcepacksPlayer;
@@ -63,8 +61,7 @@ public class BungeeResourcepacks extends Plugin implements ResourcepacksPlugin {
             Method reg = Protocol.DirectionData.class.getDeclaredMethod("registerPacket", new Class[] { int.class, Class.class });
             reg.setAccessible(true);
             try {
-                reg.invoke(Protocol.GAME.TO_CLIENT, 0x48, ResourcePackSendPacket18.class);
-                reg.invoke(Protocol.GAME.TO_CLIENT, 0x32, ResourcePackSendPacket19.class);
+                reg.invoke(Protocol.GAME.TO_CLIENT, 0x48, ResourcePackSendPacket.class);
                 
                 boolean loadingSuccessful = loadConfig();
                 
@@ -248,13 +245,7 @@ public class BungeeResourcepacks extends Plugin implements ResourcepacksPlugin {
     public void setPack(ProxiedPlayer player, ResourcePack pack) {
         int clientVersion = player.getPendingConnection().getVersion();
         if(clientVersion >= ProtocolConstants.MINECRAFT_1_8) {
-            ResourcePackSendPacket packet;
-            if(clientVersion == ProtocolConstants.MINECRAFT_1_8) {
-                packet = new ResourcePackSendPacket18(pack.getUrl(), pack.getHash());
-            } else {
-                packet = new ResourcePackSendPacket19(pack.getUrl(), pack.getHash());
-            }
-            player.unsafe().sendPacket(packet);
+            player.unsafe().sendPacket(new ResourcePackSendPacket(pack.getUrl(), pack.getHash()));
             getPackManager().setUserPack(player.getUniqueId(), pack);
             sendPackInfo(player, pack);
             getLogger().log(getLogLevel(), "Send pack " + pack.getName() + " (" + pack.getUrl() + ") to " + player.getName());
