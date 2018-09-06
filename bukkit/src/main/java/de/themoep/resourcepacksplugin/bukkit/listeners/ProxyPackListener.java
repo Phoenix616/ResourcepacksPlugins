a7abae7cdade8755ae5d2b4840d1398943187666
Phoenix616 <mail@moep.tv>
2018-09-06 14:44:24 +0100

    Add missing license

--- bukkit/src/main/java/de/themoep/resourcepacksplugin/bukkit/listeners/ProxyPackListener.java
+++ bukkit/src/main/java/de/themoep/resourcepacksplugin/bukkit/listeners/ProxyPackListener.java
@@ -1,5 +1,23 @@
 package de.themoep.resourcepacksplugin.bukkit.listeners;
 
+/*
+ * ResourcepacksPlugins - bukkit
+ * Copyright (C) 2018 Max Lee aka Phoenix616 (mail@moep.tv)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteStreams;
 import de.themoep.resourcepacksplugin.bukkit.WorldResourcepacks;
